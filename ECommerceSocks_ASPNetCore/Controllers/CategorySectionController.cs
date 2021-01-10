e6ad04e4ee5d6c8363c7c4aee01cfd38de93f438
susanasantosmoreno <Susana.Santos@tajamar365.com>
2021-01-11 00:11:09 +0100

    Home page design finished. First code of category sections.

--- ECommerceSocks_ASPNetCore/Controllers/CategorySectionController.cs
+++ ECommerceSocks_ASPNetCore/Controllers/CategorySectionController.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceSocks_ASPNetCore.Controllers {
-    public class HomeController : Controller {
-
-        // GET HOME
-        public IActionResult Index () {
+    public class CategorySectionController : Controller {
+        public IActionResult Index() {
             return View();
         }
     }
