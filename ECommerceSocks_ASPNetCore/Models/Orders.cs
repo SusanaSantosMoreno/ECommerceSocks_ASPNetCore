13189c58fbcc508cb4564d00e38c2a5d85b805fa
susanasantosmoreno <Susana.Santos@tajamar365.com>
2021-01-17 20:46:34 +0100

    Models Done

---
+++ ECommerceSocks_ASPNetCore/Models/Orders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceSocks_ASPNetCore.Models {
+    public class Orders {
+
+        public int Orders_id { get; set; }
+        public int Orders_user { get; set; }
+        public DateTime Orders_date { get; set; }
+
+        public Orders() { }
+
+        public Orders( int orders_id, int orders_user, 
+            DateTime orders_date ) {
+            Orders_id = orders_id;
+            Orders_user = orders_user;
+            Orders_date = orders_date;
+        }
+    }
+}
