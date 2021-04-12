b096317bcea5f129547f498c4b6d01b12760b0ce
Susana <susana.santos@tajamar365.com>
2021-04-12 14:21:50 +0200

    Azure blob storage implements

---
+++ ECommerceSocks_ASPNetCore/Helpers/BlobStorageService.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerceSocks_ASPNetCore.Helpers {
+    public class BlobStorageService {
+
+        IConfiguration configuration;
+
+        public BlobStorageService (IConfiguration conf) {
+            this.configuration = conf;
+        }
+
+        public async void cargarBlobs () {
+
+        }
+    }
+}
